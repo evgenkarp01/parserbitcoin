8593204336758e5fddad0745fc6472ac4205578c
evgenkarp01 <ewal@list.ru>
2018-10-30 07:47:24 +0000

    30102018

--- parse.php
+++ parse.php
@@ -1,15 +1,10 @@
-<?php 
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+<?php
 	require('simple_html_dom.php');
 	$html = file_get_html('https://double-bitcoins-legit.com/');
 	$table = $html->find('table', 0);
 	$rowData = array();
 	foreach($table->find('tr') as $row) {
-	    $flight = array();
-	    $flight[] = $row->outertext;
-	    $rowData[] = $flight;
+	    $rowData[] = $row->outertext;
 	}
 	print json_encode($rowData);
  ?>
